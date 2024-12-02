6972dadbb2d99cca298cb30d69369446050de98b
Karina <kbrilhante@gmail.com>
2024-12-02 15:05:20 -0300

    aaaaaa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # SUPER DUPER MAXI CHALLENGE
 # We are building a games collection.
 # It is not a real database since it resets every time you run the code again.
-# We are going to build a menu with multiple options for the user
+# We are going to build a menu with multiple options for the user to choose
 
 # Checklist:
 # TODO: Import games dictionary
@@ -10,3 +10,5 @@
 # TODO: Add a game to the dictionary
 # TODO: Remove a game from the dictionary
 # TODO: Edit a game in the dictionary
+
+print("hello world")
